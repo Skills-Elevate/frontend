58df8f000902a78d98f644163acd4822d68a30d6
Baptiste <batbox.longuepee@gmail.com>
2024-04-26 13:59:39 +0200

    Update connect-layout-routing.module.ts

--- src/app/layouts/connect-layout/connect-layout-routing.module.ts
+++ src/app/layouts/connect-layout/connect-layout-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnectLayoutComponent } from './connect-layout.component';
-<<<<<<< Updated upstream
 import { ProfileComponent } from "../../pages/profile/profile.component";
 import { CoursesComponent } from "../../pages/courses/courses.component";
 import { ChannelComponent } from '../../pages/channel/channel.component';
-=======
+import { CourseComponent } from 'src/app/pages/courses/course/course.component';
 import { BlogComponent } from "../../pages/blog/blog.component";
-import {ProfileComponent} from "../../pages/profile/profile.component";
-import {CoursesComponent} from "../../pages/courses/courses.component";
-import {CourseComponent} from "../../pages/courses/course/course.component";
->>>>>>> Stashed changes
+
 
 const routes: Routes = [
   {
@@ -27,13 +23,16 @@ const routes: Routes = [
         component: CoursesComponent,
       },
       {
-<<<<<<< Updated upstream
         path: 'channel/:id',
         component: ChannelComponent,
-=======
+      },
+      {
         path: 'course/:id',
         component: CourseComponent,
->>>>>>> Stashed changes
+      },
+      {
+        path: 'blog',
+        component: BlogComponent,
       },
       {
         path: 'profile',

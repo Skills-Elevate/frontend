d0011ce6faf0bf9fe1a0ed5845fe4977c1a69984
Thibaut009 <gabet.thibaut@gmail.com>
2024-06-07 12:13:34 +0200

    update navbar

--- src/app/layouts/connect-layout/header/header.component.ts
+++ src/app/layouts/connect-layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import {AuthService} from "../../../../shared/services/auth.service";
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-header',
@@ -10,11 +11,14 @@ import {AuthService} from "../../../../shared/services/auth.service";
 })
 export class HeaderComponent implements OnInit {
   isLoggedIn: boolean = false;
+  isDesktop: boolean = false;
+  showMenu: boolean = false; // Ajoutez cette variable
 
   constructor(
     private authService: AuthService,
     private cdRef: ChangeDetectorRef,
-    private router: Router
+    private router: Router,
+    private breakpointObserver: BreakpointObserver
   ) {}
 
   ngOnInit(): void {
@@ -22,23 +26,41 @@ export class HeaderComponent implements OnInit {
       this.isLoggedIn = status;
       this.cdRef.detectChanges();
     });
+
+    this.breakpointObserver.observe([Breakpoints.Handset])
+      .subscribe(result => {
+        this.isDesktop = !result.matches;
+      });
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
   }
 
   goToHome() {
+    this.toggleMenu(); // Ferme le menu
     this.router.navigate(['/']);
   }
 
-  profile() {
+  gotToCourse() {
+    this.toggleMenu(); // Ferme le menu
+    this.router.navigate(['/courses']);
+  }
+
+  goToProfile() {
+    this.toggleMenu(); // Ferme le menu
     this.router.navigate(['/profile']);
   }
 
   logout() {
+    this.toggleMenu(); // Ferme le menu
     this.authService.logout();
     this.cdRef.detectChanges();
     this.router.navigate(['/welcome']);
   }
 
   goToLogin() {
+    this.toggleMenu(); // Ferme le menu
     this.router.navigate(['/login']);
   }
 }
--- src/app/layouts/connect-layout/header/header.module.ts
+++ src/app/layouts/connect-layout/header/header.module.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { MatMenuModule } from '@angular/material/menu';
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
+    MatSidenavModule,
   ],
   exports: [
     HeaderComponent

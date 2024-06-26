cafdef612c60640048f45cec08c26085cb37b73b
Baptiste <batbox.longuepee@gmail.com>
2024-06-26 09:21:31 +0200

    Update environment.dev.ts

--- src/shared/environments/environment.dev.ts
+++ src/shared/environments/environment.dev.ts
@@ -1,8 +1,5 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-let Host = process.env['API_HOST'] || 'http://localhost';
-let Port = process.env['API_PORT'] || '3000';
+let Host = 'http://localhost';
+let Port = '3000';
 
 export const environment = {
   production: true,
